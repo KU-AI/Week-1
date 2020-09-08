828d6cd77e2dad8b2d4cf35a03120e281f5b9076
HJH <jungho-hong@korea.ac.kr>
2020-09-08 14:26:52 +0900

    added dataset script

---
+++ dataset/download.sh
@@ -0,0 +1,17 @@
+
+#pip install gdown
+
+# Weather classification
+# gdown https://drive.google.com/uc?id=13VSXBZPra_qt3FtcgkA6IBUIZqsV7iHS
+
+# Monkeys
+# gdown https://drive.google.com/uc?id=1g7aMPWe78UXgUR0jEDZArC29KGbJ8qag
+
+# Dogs and cat
+gdown https://drive.google.com/uc?id=1alXT3P-dO7HuXirwC1BVmYrSjTEY6QZM
+
+# Dog and cat and panda
+# gdown https://drive.google.com/uc?id=1-BZXfw6Lh8dwYmKGvGbsjusxqrOsFO3a
+
+# Artworks
+# gdown https://drive.google.com/uc?id=1QamfG4AaWh0CJ6eeZ_Vvxn0KJdLpmKfB
